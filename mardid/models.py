143a91706c1305dbafc844ee7a08b3a26b96a337
UpsonP <upsonp@gmail.com>
2026-01-20 15:23:50 -0400

    Related to issue #21, added all database models

--- mardid/models.py
+++ mardid/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import TextField
 from django.utils.translation import gettext_lazy as _
 
 ############################################################################################
@@ -27,7 +28,7 @@ class Platform(models.Model):
     platform = models.CharField(max_length=100, db_column='PLATFORM', help_text=_("Platform name or description"))
     country = models.ForeignKey(Country, related_name="platforms", on_delete=models.CASCADE, blank=True, null=True, db_column='COUNTRY_SEQ')
     call_sign = models.CharField(max_length=10, db_column='CALL_SIGN', blank=True, null=True, help_text=_("Platform call sign, if applicable"))
-    max_speed = models.IntegerField(max_length=3, db_column='MAXIMUM_SPEED', blank=True, null=True, help_text=_("Maximum speed of the platform in knots"))
+    max_speed = models.IntegerField(db_column='MAXIMUM_SPEED', blank=True, null=True, help_text=_("Maximum speed of the platform in knots"))
 
     class Meta:
         db_table = 'LU_PLATFORM'
@@ -220,13 +221,65 @@ class Contact(models.Model):
 
 class MissionMetadata(models.Model):
     id = models.AutoField(primary_key=True, db_column='MISSION_METADATA_SEQ')
-
-    start_date = models.DateField(db_column='START_DATE')
-    end_date = models.DateField(db_column='END_DATE')
+    mission_name = models.CharField(max_length=25, db_column='MISSION_NAME')
+    platform = models.ForeignKey(Platform, related_name='mission_metadata', on_delete=models.CASCADE, db_column='PLATFORM_SEQ')
+    nodc_descriptor = models.CharField(max_length=15, db_column='NODC_DESCRIPTOR', blank=True, null=True)
+    start_date = models.DateField(db_column='START_DATE', blank=True, null=True)
+    end_date = models.DateField(db_column='END_DATE', blank=True, null=True)
+    contact_group = models.ForeignKey(GroupRole, related_name='mission_metadata', on_delete=models.CASCADE, db_column='CONTACT_GROUP_SEQ')
+    program = models.ForeignKey(Program, related_name='mission_metadata', on_delete=models.CASCADE, db_column='PROGRAM_SEQ')
+    institute = models.ForeignKey(Institute, related_name='mission_metadata', on_delete=models.CASCADE, db_column='INSTITCH_SEQ')
+    project_planning_tool_id = models.IntegerField(db_column='PPT_ID', blank=True, null=True)
 
     class Meta:
         db_table = 'MISSION_METADATA'
 
+
+class Equipment(models.Model):
+    id = models.AutoField(primary_key=True, db_column='EQUIPMENT_SEQ')
+    equipment_type = models.ForeignKey(InstrumentType, related_name='equipment', on_delete=models.CASCADE, db_column='EQUIPMENT_TYPE')
+    equipment_number = models.CharField(max_length=25, blank=True, null=True, db_column='EQUIPMENT_NUMBER')
+    serial_number = models.CharField(max_length=50, blank=True, null=True, db_column='SERIAL_NUMBER')
+    metadata = models.ForeignKey(MissionMetadata, related_name='equipment', on_delete=models.CASCADE, db_column='MISSION_METADATA_SEQ')
+    deployment_date = models.DateField(db_column='DEPLOYMENT_DATE')
+    recovery_date = models.DateField(db_column='RECOVERY_DATE')
+    start_lat = models.FloatField(db_column='START_LATITUDE', blank=True, null=True)
+    start_lon = models.FloatField(db_column='START_LONGITUDE', blank=True, null=True)
+    end_lat = models.FloatField(db_column='END_LATITUDE', blank=True, null=True)
+    end_lon = models.FloatField(db_column='END_LONGITUDE', blank=True, null=True)
+    date_entered = models.DateField(db_column='DATE_ENTERED')
+    entered_by = models.ForeignKey(Contact, related_name='equipment', on_delete=models.CASCADE, db_column='ENTERED_BY_ID')
+    recovery_metadata = models.ForeignKey(MissionMetadata, related_name='equipment_recoveries', blank=True, null=True, on_delete=models.CASCADE, db_column='RECOVERY_MISSION_METADATA_SEQ')
+
+
+class DataCollected(models.Model):
+    id = models.AutoField(primary_key=True, db_column='DATA_COLLECTED_SEQ')
+    equipment = models.ForeignKey(Equipment, related_name='data_collected', on_delete=models.CASCADE, db_column='EQUIPMENT_SEQ')
+    instrument_type = models.ForeignKey(InstrumentType, related_name='data_collected', on_delete=models.CASCADE, db_column='INSTRUMENT_TYPE_SEQ')
+    principal_investigator = models.ForeignKey(Contact, related_name='data_collected_pis', on_delete=models.CASCADE, db_column='PI_SEQ')
+    parameter_type = models.ForeignKey(ParameterType, related_name='data_collected', on_delete=models.CASCADE, db_column='PARAMETER_TYPE_SEQ')
+    serial_number = models.CharField(max_length=35, db_column='SERIAL_NUMBER', blank=True, null=True)
+    data_processor = models.ForeignKey(Contact, related_name='data_collected_processors', on_delete=models.CASCADE, db_column='DATA_PROCESSOR_SEQ')
+    processing_state = models.ForeignKey(ProcessingState, related_name='data_collected', on_delete=models.CASCADE, db_column='PROCESSING_STATE_SEQ')
+    date_entered = models.DateField(db_column='DATE_ENTERED')
+    entered_by = models.CharField(max_length=100, db_column='ENTERED_BY')
+    date_received = models.DateField(db_column='DATE_RECEIVED')
+    loaded_to_odf_archive = models.BooleanField(default=False, db_column='LOADED_TO_ODF_ARCHIVE')
+    sent_to_meds = models.BooleanField(default=False, db_column='SENT_TO_MEDS')
+    organization = models.ForeignKey(Organization, related_name='data_collected', on_delete=models.CASCADE, db_column='ORGANIZATION_SEQ')
+    division = models.ForeignKey(Division, related_name='data_collected', on_delete=models.CASCADE, db_column='DIVISION_SEQ', blank=True, null=True)
+
+
+class File(models.Model):
+    id = models.AutoField(primary_key=True, db_column='FILE_SEQ')
+    metadata = models.ForeignKey(MissionMetadata, related_name="files", on_delete=models.CASCADE, db_column='MISSION_METADATA_SEQ')
+    data_collected = models.ForeignKey(DataCollected, related_name="files", on_delete=models.CASCADE, db_column='DATA_COLLECTED_SEQ')
+    file_type = models.ForeignKey(FileType, related_name="files", on_delete=models.CASCADE, db_column='FILE_TYPE_SEQ')
+    folder_path = models.ForeignKey(FolderPath, related_name="files", on_delete=models.CASCADE, db_column='FOLDER_PATH_SEQ')
+    relative_path = models.CharField(max_length=200, db_column='RELATIVE_PATH')
+    file_name = models.CharField(max_length=200, db_column='FILE_NAME')
+    date_submitted = models.DateField(db_column='DATE_SUBMITTED')
+    submitted_by = models.ForeignKey(Contact, related_name="files", on_delete=models.CASCADE, db_column='SUBMITTED_BY_SEQ')
 
 ############################################################################################
 ## Many-to-many Tables
@@ -242,3 +295,28 @@ class MissionRegion(models.Model):
 
     def __str__(self):
         return f"{self.mission} - {self.geographic_region}"
+
+class FileComment(models.Model):
+    id = models.AutoField(primary_key=True, db_column='FILE_COMMENT_SEQ')
+    author = models.ForeignKey(Contact, related_name="file_comments", on_delete=models.CASCADE, db_column='AUTHOR_SEQ')
+    file = models.ForeignKey(File, related_name="file_comments", on_delete=models.CASCADE, db_column='FILE_SEQ')
+    comment = TextField(db_column='COMMENT')
+    date = models.DateField(db_column='FILE_COMMENT_DATE')
+
+    class Meta:
+        db_table = 'FILE_COMMENT'
+        ordering = ['date']
+
+class MissionComment(models.Model):
+    id = models.AutoField(primary_key=True, db_column='MISSION_COMMENT_SEQ')
+    mission_comment = models.TextField(db_column='MISSION_COMMENT')
+    author = models.ForeignKey(Contact, related_name="mission_comments", on_delete=models.CASCADE, db_column='AUTHOR_SEQ')
+    metadata = models.ForeignKey(MissionMetadata, related_name='mission_comments', on_delete=models.CASCADE, db_column='MISSION_METADATA_SEQ')
+    comment_date = models.DateField(db_column='MISSION_COMMENT_DATE')
+
+
+class EquipmentProject(models.Model):
+    id = models.AutoField(primary_key=True, db_column='EQUIPMENT_PROJECT_SEQ')
+    equipment = models.ForeignKey(Equipment, related_name="equipment_projects", on_delete=models.CASCADE, db_column='EQUIPMENT_SEQ')
+    metadata = models.ForeignKey(MissionMetadata, related_name="equipment_projects", on_delete=models.CASCADE, db_column='MISSION_METADATA_SEQ')
+    project = models.ForeignKey(Project, related_name="equipment_projects", on_delete=models.CASCADE, db_column='PROJECT_SEQ')
