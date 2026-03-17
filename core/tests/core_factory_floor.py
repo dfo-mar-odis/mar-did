c573193f3272e6ad324373d0b209a696a6caac58
Patrick Upson <Patrick.Upson@dfo-mpo.gc.ca>
2026-03-17 09:26:58 -0300

    added geographic regions to mission legs on mission form

--- core/tests/core_factory_floor.py
+++ core/tests/core_factory_floor.py
@@ -1,6 +1,7 @@
 import datetime
 
 import factory
+from django.test import TestCase
 from faker import Faker
 
 from django.contrib.auth import models as auth
@@ -8,88 +9,39 @@ from django.contrib.auth import models as auth
 from core import models
 
 fake = Faker()
-class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = auth.User
 
-    first_name = factory.LazyFunction(lambda: fake.name())
-    last_name = factory.LazyFunction(lambda: fake.name())
-    user_name = factory.LazyFunction(lambda o: f"{o.last_name.lower()}{o.first_name.lower()[1]}")
+class MardidTestCase(TestCase):
+    fixtures = ['test_countries', 'test_platforms', 'test_programs', 'test_regions']
 
-
-class CruiseFactory(factory.django.DjangoModelFactory):
+class MissionFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Missions
-        exclude = ('leg', 'year',)
 
-    start_date = factory.LazyFunction(lambda: Faker().date_object())
-    end_date = factory.LazyAttribute(lambda obj: obj.start_date + datetime.timedelta(days=21))
     platform = factory.LazyFunction(
-        lambda: models.Platforms.objects.order_by('?').first() or PlatformFactory()
+        lambda: models.Platforms.objects.order_by('?').first()
     )
-    leg = factory.LazyFunction(lambda: f"{Faker().random_int(min=1, max=999):03}")
-    year = factory.LazyAttribute(lambda o: datetime.datetime.strftime(o.start_date, '%Y'))
+
+    program = factory.LazyFunction(
+        lambda: models.Programs.objects.order_by('?').first()
+    )
+
     name = factory.LazyAttribute(
-        lambda o: f"{Faker().random_element(['CAR', 'DY', 'JC'])}{o.year}{o.leg}"
-    )
-    descriptor = factory.LazyAttribute(
-        lambda o: f"{o.platform.ship_code}{o.year}{o.leg}"
+        lambda o: f"{Faker().random_element(['CAR', 'DY', 'JC'])}{Faker().random_int(min=2000, max=2030):04}{Faker().random_int(min=1, max=999):03}"
     )
 
-    @factory.post_generation
-    def chief_scientists(self, create, extracted, **kwargs):
-        if not create:
-            return
-        if extracted:
-            self.chief_scientists.set(extracted)
-        else:
-            group = auth.Group.objects.filter(name='Chief Scientists').first()
-            if group and group.user_set.exists():
-                self.chief_scientists.set(group.user_set.order_by('?')[:Faker().random_int(min=1, max=2)])
-            else:
-                self.chief_scientists.set(UserFactory.create_batch(size=Faker().random_int(min=1, max=2)))
+class MissionLegFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Legs
 
-    @factory.post_generation
-    def data_managers(self, create, extracted, **kwargs):
-        if not create:
-            return
-        if extracted:
-            self.data_managers.set(extracted)
-        else:
-            group = auth.Group.objects.filter(name='Data Managers').first()
-            if group and group.user_set.exists():
-                self.data_managers.set(group.user_set.order_by('?')[:Faker().random_int(min=1, max=2)])
-
-    @factory.post_generation
-    def locations(self, create, extracted, **kwargs):
-        if not create:
-            return
-        if extracted:
-            self.locations.set(extracted)
-        else:
-            locations = models.GeographicRegions.objects.all().order_by('?')
-            if locations and locations.exists():
-                self.locations.set(locations[:Faker().random_int(min=1, max=2)])
-
-
-    @factory.post_generation
-    def programs(self, create, extracted, **kwargs):
-        if not create:
-            return
-        if extracted:
-            self.programs.set(extracted)
-        else:
-            programs = models.Programs.objects.all().order_by('?')
-            if programs and programs.exists():
-                self.programs.set(programs[:Faker().random_int(min=1, max=2)])
-
-    @factory.post_generation
-    def datasets(self, create, extracted, **kwargs):
-        if not create:
-            return
-        if extracted:
-            self.datasets.set(extracted)
-        else:
-            datasets = models.Dataset.objects.all().order_by('?')
-            if datasets and datasets.exists():
-                self.datasets.set(datasets[:Faker().random_int(min=5, max=10)])
+    mission = factory.SubFactory(MissionFactory)
+    number = factory.Sequence(lambda n: n + 1)
+    start_date = factory.LazyFunction(
+        lambda: datetime.date(
+            year=fake.random_int(min=2000, max=2030),
+            month=fake.random_int(min=1, max=12),
+            day=fake.random_int(min=1, max=28)
+        )
+    )
+    end_date = factory.LazyAttribute(
+        lambda obj: obj.start_date + datetime.timedelta(days=fake.random_int(min=1, max=30))
+    )
