78d242528c7e2b11f104d303f112fb3bfc4fe9fc
Patrick Upson <upsonp@gmail.com>
2026-04-17 10:35:55 -0300

    added a bulk IN directory builder core.utils.bulk_upload

---
+++ core/utils/bulk_upload.py
@@ -0,0 +1,23 @@
+import logging
+from pathlib import Path
+
+from django.conf import settings
+
+from core.models import Missions
+
+logger = logging.getLogger('mardid')
+
+def build_file_structure(mission: Missions):
+    input_path = Path(settings.MEDIA_IN, mission.mission_path)
+    logger.info(f"Building file structure for mission: {mission.name}")
+
+    if not input_path.exists():
+        logger.debug(f"Creating mission input directory: {input_path}")
+        input_path.mkdir(parents=True)
+
+    for dataset in mission.datasets.all():
+        for location in dataset.datatype.locations.all():
+            datatype_path = Path(input_path, location.input_dir)
+            if not datatype_path.exists():
+                logger.debug(f"Creating datatype input directory: {datatype_path}")
+                datatype_path.mkdir(parents=True)
