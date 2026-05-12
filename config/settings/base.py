671cced189e50b6b5964bdc0ec94715f3c2b5029
Patrick Upson <Patrick.Upson@dfo-mpo.gc.ca>
2026-05-12 12:35:26 -0300

    fixed missing unit tests from send_mail branch

--- config/settings/base.py
+++ config/settings/base.py
@@ -213,8 +213,10 @@ LOGGING = {
 }
 
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT =os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST=os.getenv('EMAIL_HOST')
+EMAIL_PORT=os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+EMAIL_SSL_CERTFILE = os.getenv('EMAIL_SSL_CERTFILE')
+EMAIL_SSL_KEYFILE = os.getenv('EMAIL_SSL_KEYFILE')
