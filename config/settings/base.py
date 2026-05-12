6534ef002970230922518e6cbcf28d33a8773239
Patrick Upson <patrick.upson@dfo-mpo.gc.ca>
2026-05-12 12:57:16 -0230

    added email capability to mardid

--- config/settings/base.py
+++ config/settings/base.py
@@ -213,9 +213,8 @@ LOGGING = {
 }
 
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT =os.getenv('EMAIL_PORT')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST=os.getenv('EMAIL_HOST')
+EMAIL_PORT=os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
