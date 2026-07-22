e81a0527e934a93ebf9ad6c737dd1a2b9048a0df
UpsonP <upsonp@gmail.com>
2026-07-22 09:59:49 -0300

    wrong spelling for htmx-indicator

--- core/views/forms/form_dataset_submission.py
+++ core/views/forms/form_dataset_submission.py
@@ -66,7 +66,7 @@ class DatasetSubmissionArchiveForm(forms.Form):
         btn_submit_attrs = {
             'title': _("Archive Previously Loaded Files"),
             'hx-target': "#div_id_archive_message_form",
-            'hx-indicator': ".hx-indicator",
+            'hx-indicator': ".htmx-indicator",
             'hx-post': submit_url
         }
 
